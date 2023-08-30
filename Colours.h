06d384b6be0c5c76cb46b84e71e8eff843d489f1
KuchinaMA <m.kuchina05@mail.ru>
2023-08-30 17:51:46 +0300

    Add colours

---
+++ Colours.h
@@ -0,0 +1,13 @@
+#ifndef COLOURS_H_INCLUDED
+#define COLOURS_H_INCLUDED
+
+#include <stdio.h>
+
+#define WHITE(str) "\x1B[37m" str "\x1B[0m"
+#define YELLOW(str) "\x1B[33m" str "\x1B[0m"
+#define RED(str) "\x1B[31m" str "\x1B[0m"
+
+//#define COLOR_RED(str) "\x1B[31m" str "\x1B[0m"
+
+
+#endif // COLOURS_H_INCLUDED
